e27aebc37ea693817ba3d1ec1b6658113b9bd575
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2016-08-12 16:04:52 +0200

    Fin semaine 1

---
+++ AddressBookSymfony/src/AppBundle/Controller/SocieteController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * @Route("/societe")
+ */
+class SocieteController extends Controller
+{
+
+    /**
+     * @return \AppBundle\Repository\SocieteRepository
+     */
+    protected function getRepository()
+    {
+        return $this->getDoctrine()->getRepository('AppBundle:Societe');
+    }
+
+    /**
+     * @Route("/")
+     */
+    public function listAction()
+    {
+        return $this->render('AppBundle:Societe:list.html.twig', array(
+            'societes' => $this->getRepository()->findAll()
+        ));
+    }
+
+    /**
+     * @Route("/{id}")
+     */
+    public function showAction($id)
+    {
+        return $this->render('AppBundle:Societe:show.html.twig', array(
+            'societe' => $this->getRepository()->find($id)
+        ));
+    }
+
+}
---
+++ AddressBookSymfony/src/Prepavenir/AliceBundle/Command/AliceLoadCommand.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Prepavenir\AliceBundle\Command;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\SchemaTool;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AliceLoadCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('alice:load')
+            ->setDescription('Charge les fichiers de fixture Alice')
+            ->addArgument('fixturePath', InputArgument::REQUIRED, 'Le chemin vers le fichier YAML')
+            ->addOption('drop', null, InputOption::VALUE_NONE, 'Supprime les tables')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $fixturePath = $input->getArgument('fixturePath');
+
+        $projectPath = $this->getContainer()->getParameter('kernel.root_dir') . '/../';
+        $fixturePath = $projectPath . $fixturePath;
+
+        if (!file_exists($fixturePath)) {
+            $output->writeln('Erreur : le chemin spécifié est introuvable');
+            return;
+        }
+
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+
+        if ($input->getOption('drop')) {
+            $tool = new SchemaTool($em);
+            $metadatas = $em->getMetadataFactory()->getAllMetadata();
+            $tool->dropSchema($metadatas);
+            $tool->createSchema($metadatas);
+        }
+
+        $objects = \Nelmio\Alice\Fixtures::load($fixturePath, $em, [
+            'locale' => 'fr_FR',
+            'seed' => time()
+        ]);
+
+        $output->writeln('Objets insérés : ' . count($objects));
+    }
+
+}
